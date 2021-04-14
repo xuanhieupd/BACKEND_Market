c9b441c7eae52bf3e49abac3def1b8c53ba7b4e8
XuanHieu <xuanhieu.pd@gmail.com>
2021-04-14 16:47:24 +0700

    no message

--- app/Modules/Chat/ControllerAPI/ConversationController.php
+++ app/Modules/Chat/ControllerAPI/ConversationController.php
@@ -10,28 +10,4 @@ use Illuminate\Http\Request;
 class ConversationController extends CreateController
 {
 
-    /**
-     * @param Request $request
-     * @return ConversationResource|mixed
-     * @throws \Throwable
-     */
-    public function actionIndex(Request $request)
-    {
-        try {
-            $this->request = $request;
-
-            $from = $this->from();
-            $to = $this->to();
-
-            $conversationInfo = Chat::conversations()->between($from, $to);
-            if (!$conversationInfo) return $this->responseError('Không thuộc cuộc hội thoại nào');
-
-            return new ConversationResource($conversationInfo);
-        } catch (ParticipantException $participantException) {
-            return $this->responseError('ParticipantException');
-        } catch (\Exception $e) {
-            return $this->responseError('Có lỗi khi tìm kiếm cuộc hội thoại');
-        }
-    }
-
 }
