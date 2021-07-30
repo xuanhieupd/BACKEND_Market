34f29bb7efb93db6e7245384181c8106055e88b8
DucLV <cntt0401.luuvietduc@gmail.com>
2021-07-30 10:28:02 +0700

    search product without relate product if store in no follow

--- app/Modules/Product/ControllerAPI/ProductsController.php
+++ app/Modules/Product/ControllerAPI/ProductsController.php
@@ -11,11 +11,14 @@
 namespace App\Modules\Product\ControllerAPI;
 
 use App\Base\AbstractController;
+use App\GlobalConstants;
+use App\Modules\Follower\Models\Entities\Follower;
 use App\Modules\Product\Models\Repositories\Contracts\ProductInterface;
 use App\Modules\Product\Models\Repositories\Eloquents\ProductRepository;
 use App\Modules\Product\Resources\ProductResource;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Auth;
 
 class ProductsController extends AbstractController
 {
@@ -51,11 +54,30 @@ class ProductsController extends AbstractController
             return $currentPage;
         });
 
+        $followStores = Follower::query()
+//            ->whereIn('store_id', $storeIds)
+            ->where('user_id', Auth::id())
+            ->where('status_id', GlobalConstants::STATUS_ACTIVE)
+            ->select('store_id')
+            ->get()
+            ->pluck('store_id')
+            ->toArray();
+
+
         $products = $this->productRepo->getProducts()
             ->select(array('product_id', 'store_id', 'sku', 'title', 'whole_price', 'retail_price', 'import_price', 'collaborator_price', 'attachment_id'))
             ->filter($request->all())
             ->with(array('productAttachment'))
             ->public()
+            ->where(function ($q) use ($followStores) {
+                $q->where(function($q1){
+                    $q1->where('assign_id', 0);
+                })
+                ->orWhere(function($q2) use ($followStores){
+                    $q2->where('assign_id', '<>' ,0)
+                    ->whereIn('store_id', $followStores);
+                });
+            })
             ->search($request->get('q'))
             ->sortBy($request->get('orderId'))
             ->simplePaginate(20);
